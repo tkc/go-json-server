93d537be3d72e5084397feb1b8654f4cb32a5da2
tkc <181991+tkc@users.noreply.github.com>
2018-11-05 08:31:46 +0900

    Add headerType

--- go-json-server.go
+++ go-json-server.go
@@ -125,6 +125,12 @@ func main() {
 }
 
 func Response(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set( "Access-Control-Allow-Origin", "*" )
+	w.Header().Set( "Access-Control-Allow-Credentials", "true" )
+	w.Header().Set( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" )
+	w.Header().Set( "Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS" )
+
 	r.ParseForm()
 	for _, ep := range api.Endpoints {
 		if r.URL.Path == ep.Path && r.Method == ep.Method {
